3ea7f7e301c84d6cb373c2750fcd18f0d68ddad9
shortfat <aevrtlinda@gmail.com>
2022-04-24 16:01:28 -0700

    test

--- item.cs
+++ item.cs
@@ -1,7 +1,7 @@
 namespace Shopping
 {
     public class item
-    {   
+    {   public int count;
         private product itemProduct;
         private int quantity;
         public bool updateQuantity(int newQuantity){}
